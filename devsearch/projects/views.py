b2ac7c557e90f0e6bac2822b991842b7dafcabb8
mohankrishnan16 <mohankrishnann601@gmail.com>
2022-10-08 22:36:40 +0530

    sec 3: Building on our database completion

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project, Tag
 # Create your views here.
 #static url call response
 projectList=[
@@ -22,16 +22,19 @@ projectList=[
 ]
 def projects(request):
     #return HttpResponse('Here are the projects!')
-    page='projects'
-    number=10
-    context={'page':page,'number':number,'projects':projectList}
+    """page='projects'
+    number=10"""
+    #context={'page':page,'number':number,'projects':projectList}
+    projects=Project.objects.all()
+    context={'projects':projects}
     #return render(request,'projects/projects.html',context)
     return render(request,'projects/projects.html',context)
 #dynamic url call response
 def project(request,pk):
     #return HttpResponse('Single project URL'+' '+str(pk))
-    for single_project in projectList:
+    """for single_project in projectList:
         if single_project['id']==pk:
-            project_obj=single_project
-
-    return render(request,'projects/single-project.html',project_obj)
+            project_obj=single_project"""
+    project_obj=Project.objects.get(id=pk)
+    tags=project_obj.tags.all()
+    return render(request,'projects/single-project.html',{'project_obj':project_obj,'tags':tags})
